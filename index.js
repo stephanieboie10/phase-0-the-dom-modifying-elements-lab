02fd4b49882c10fbe2f95f7bcfdc9e32fc95fa98
Stephanie Boie <stephanieboie10@gmail.com>
2023-04-08 13:07:28 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.innerHTML = "{$<h1>} Stephanie is the champion";
+
+const element = document.getElementById("main");
+element.remove('main');
